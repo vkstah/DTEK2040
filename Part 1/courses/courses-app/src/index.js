954506b9bbb786227e28f1cd1e7ca614aebe6705
Vili Ståhlberg <vkstah@utu.fi>
2022-02-24 17:37:47 +0200

    Completed exercise 1.5

--- Part 1/courses/courses-app/src/index.js
+++ Part 1/courses/courses-app/src/index.js
@@ -6,27 +6,29 @@ import Contents from './components/Contents'
 import Total from './components/Total'
 
 const App = () => {
-  const course = 'Superadvanced web and mobile programming'
-  const parts =[
-    {
-      name: 'Basics of React',
-      exercises: 8
-    },
-    {
-      name: 'Using props',
-      exercises: 10
-    },
-    {
-      name: 'Component states',
-      exercises: 12
-    }
-  ]
+  const course = {
+    name: 'Superadvanced web and mobile programming',
+    parts: [
+      {
+        name: 'Basics of React',
+        exercises: 8
+      },
+      {
+        name: 'Using props',
+        exercises: 10
+      },
+      {
+        name: 'Component states',
+        exercises: 12
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Contents parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Contents parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
